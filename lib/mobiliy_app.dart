63c0c0c1e11f7efd7ef2af7fc2f81eb5021e4345
Ahmed Mahdaly <ahmed.mahdaly86@gmail.com>
2024-12-01 22:41:52 +0200

    Connectivity_plus Done

--- lib/mobiliy_app.dart
+++ lib/mobiliy_app.dart
@@ -1,19 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:mobilia/core/app/connectivity_controller.dart';
 import 'package:mobilia/core/app/env_variables.dart';
+import 'package:mobilia/core/common/screens/no_network_screen.dart';
+import 'package:mobilia/test.dart';
 
 class MobiliaApp extends StatelessWidget {
   const MobiliaApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: EnvVariables.instance.debugMode,
-      title: 'MOBILIA',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const Scaffold(),
+    /// في حالة وجود انترنت متصل
+    return ValueListenableBuilder(
+      valueListenable: ConnectivityController.instance.isConnected,
+      builder: (_, value, __) {
+        if (value) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: EnvVariables.instance.debugMode,
+            title: 'MOBILIA',
+            builder: (context, widget) {
+              return Scaffold(
+                body: Builder(
+                  builder: (context) {
+                    ConnectivityController.instance.init();
+                    return widget!;
+                  },
+                ),
+              );
+            },
+            home: const Test(),
+          );
+        } else {
+          /// في حالة عدم وجود انترنت متصل
+          return MaterialApp(
+            debugShowCheckedModeBanner: EnvVariables.instance.debugMode,
+            title: 'No NetWork ',
+            home: const NoNetworkScreen(),
+          );
+        }
+      },
     );
   }
 }
