32bd3ddaecacb1af795723fcd4c10b70764ebb7a
Ahmed Mahdaly <ahmed.mahdaly86@gmail.com>
2024-12-02 00:40:33 +0200

    Splash screen Done

--- lib/mobiliy_app.dart
+++ lib/mobiliy_app.dart
@@ -1,11 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:mobilia/core/app/connectivity_controller.dart';
 import 'package:mobilia/core/app/env_variables.dart';
 import 'package:mobilia/core/common/screens/no_network_screen.dart';
 import 'package:mobilia/test.dart';
 
-class MobiliaApp extends StatelessWidget {
+class MobiliaApp extends StatefulWidget {
   const MobiliaApp({super.key});
+
+  @override
+  State<MobiliaApp> createState() => _MobiliaAppState();
+}
+
+class _MobiliaAppState extends State<MobiliaApp> {
+  @override
+  void initState() {
+    super.initState();
+    initialization();
+  }
+
+  Future<void> initialization() async {
+    // ignore: inference_failure_on_instance_creation
+    await Future.delayed(
+      const Duration(
+        seconds: 3,
+      ),
+    );
+
+    FlutterNativeSplash.remove();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +39,7 @@ class MobiliaApp extends StatelessWidget {
         if (value) {
           return MaterialApp(
             debugShowCheckedModeBanner: EnvVariables.instance.debugMode,
-            title: 'MOBILIA',
+            title: 'MoBilia',
             builder: (context, widget) {
               return Scaffold(
                 body: Builder(
