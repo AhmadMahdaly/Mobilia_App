a715b3a89e761ad55b814439b8340b530745ae81
Ahmed Mahdaly <ahmed.mahdaly86@gmail.com>
2024-12-04 15:29:37 +0200

    Add Cubit & get_it & shared_pref And Freezed

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,12 @@ import 'dart:io';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:mobilia/core/app/bloc_observer.dart';
 import 'package:mobilia/core/app/connectivity_controller.dart';
 import 'package:mobilia/core/app/env_variables.dart';
+import 'package:mobilia/core/service/shared_preferances/shared_pref.dart';
 import 'package:mobilia/mobiliy_app.dart';
 
 Future<void> main() async {
@@ -35,6 +38,12 @@ Future<void> main() async {
           ),
         )
       : await Firebase.initializeApp();
+
+  /// BloC observer
+  Bloc.observer = AppBlocObserver();
+
+  /// Shared Pref
+  await SharedPref().instantiatePreferences();
 
   /// To make Portrait
   await SystemChrome.setPreferredOrientations(
